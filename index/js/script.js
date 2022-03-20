af5ae441ae8cda9fec2c1916ed96abec46525cf3
Lubomir Georgiev <86322070+LRGeorgiev@users.noreply.github.com>
2022-03-20 16:11:00 +0200

    Add js folder

---
+++ index/js/script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", () =>{
+        document.querySelector(".main").classList.remove("hidden");
+        document.querySelector(".home-section").classList.add("active");
+ });
