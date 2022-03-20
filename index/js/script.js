ffb0d761764ae08256b2c8b91b60e248506e6b85
Lubomir Georgiev <86322070+LRGeorgiev@users.noreply.github.com>
2022-03-20 16:10:41 +0200

    Delete script.js

--- index/js/script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("load", () =>{
-        document.querySelector(".main").classList.remove("hidden");
-        document.querySelector(".home-section").classList.add("active");
- });
