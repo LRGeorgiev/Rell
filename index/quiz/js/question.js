9fbc7aee102551e5b188afbb63421279cee55b3d
Lubomir Georgiev <86322070+LRGeorgiev@users.noreply.github.com>
2022-03-20 16:10:16 +0200

    Add files via upload

---
+++ index/quiz/js/question.js
@@ -0,0 +1,60 @@
+const quiz = [
+  {
+  	q:'In which order are all the musical folklore areas in Bulgaria listed ?',
+  	options:['Severyashka, Thracian, Shopska, Dobrudzha, Pirin, Rhodope' , 'Danube, Stara Planina, Thracian, Shopska, Varna', 'Varna, Severnyashka, Kotlen, Strandzha, Thracian','Pirin, Northern, Thracian'],
+  	answer:0,
+	img:'quiz/img/folkloree.png'
+  },
+  {
+  	q:'What is the most extensive folklore area in Bulgaria?',
+  	options:['Dobrudzha','Severnyashka','Rodopska','Thracian'],
+  	answer:3,
+	img:'quiz/img/bg map.jpg'
+  },
+  {
+  	q:'Which folklore area covers Northwestern and Central Northern Bulgaria?',
+  	options:['Rhodope folklore region','Pirin folklore region','Severnyashka folklore area','Thracian folklore region'],
+  	answer:2,
+	img:'quiz/img/men.png'
+  },
+  {
+  	q:'Which folklore area is also known as the "Granary of Bulgaria"?',
+  	options:['Northern folklore area','Thracian folklore region','Dobrudzha folklore region','None of the above'],
+  	answer:2,
+	img: 'quiz/img/granary.jpg'
+  },
+  {
+  	q:'Which folklore area is characterized by singing at second intervals?',
+  	options:['Rhodope','Pirinska','Severnyashka','Shopska'],
+  	answer:3,
+	img: 'quiz/img/dance.jpg'
+  },
+  {
+    q:'The zurna is a typical tool for ..?',
+    options:['The northern folklore area','The Thracian folklore region','Dobrudzha folklore region','Pirin folklore region'],
+    answer:3,
+	img: 'quiz/img/zurnaa.jpg'
+  },
+  {
+    q:'From which folklore area is the song "Izlel e delio haidutin"?',
+    options:['Rhodope','Thracian','Dobrudzha','Pirin'],
+    answer:0,
+	img: 'quiz/img/haidutin.jpg'
+  },
+  {
+    q:'In which area of music and folklore is it sung mostly in two voices?',
+    options:['Rhodope','Thracian','Dobrudzha','Pirin'],
+    answer:3,
+  },
+  {
+    q:'In which of the three areas is the tarambuka played?',
+    options:['Rhodope','Thracian','Dobrudzha','Pirin'],
+    answer:3,
+	img: 'quiz/img/tarambuka1.jpg'
+  },
+  {
+    q:'How many music and folklore areas are there in Bulgaria?',
+    options:['4','5','6','7'],
+    answer:2,
+  }
+ ]
